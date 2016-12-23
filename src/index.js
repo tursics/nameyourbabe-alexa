f0f3c1d49a245db7a3fce0c840386439aaa3c157
Thomas Tursics <thomas@tursics.de>
2016-12-23 17:15:14 +0100

    first version

--- src/index.js
+++ src/index.js
@@ -11,17 +11,17 @@
 'use strict';
 
 const Alexa = require('alexa-sdk');
-const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 var names = require('./names');
+
+const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
 const languageStrings = {
 	'de-DE': {
 		translation: {
 			SKILL_NAME: 'Baby Namen',
-			WELCOME_MESSAGE: 'Willkommen bei %s. Du kannst beispielsweise die Frage stellen: Welche Rezepte gibt es für eine Truhe? ... Nun, womit kann ich dir helfen?',
+			WELCOME_MESSAGE: 'Willkommen bei %s. Du kannst beispielsweise sagen: Nenne mir einen Vornamen. Ich kenne über 5000, also los geht\'s.',
             WELCOME_REPROMT: 'Wenn du wissen möchtest, was du sagen kannst, sag einfach „Hilf mir“.',
-            GET_FACT_MESSAGE: 'Hier sind deine Fakten: ',
-            HELP_MESSAGE: 'Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?',
+            HELP_MESSAGE: 'Ich kenne über 5000 beliebte Vornamen. Du kannst sagen, „Nenne mir einen Vornamen“, und ich verrate dir einen. Oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?',
             HELP_REPROMPT: 'Wie kann ich dir helfen?',
             STOP_MESSAGE: 'Auf Wiedersehen!',
         },
@@ -29,45 +29,51 @@ const languageStrings = {
 };
 
 const handlers = {
-	'LaunchRequest': function () {
-		this.attributes['speechOutput'] = this.t("WELCOME_MESSAGE", this.t("SKILL_NAME"));
-		// If the user either does not reply to the welcome message or says something that is not
-		// understood, they will be prompted again with this text.
-		this.attributes['repromptSpeech'] = this.t("WELCOME_REPROMT");
-		this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
-   },
+    'LaunchRequest': function () {
+        const speechOutput = this.t('WELCOME_MESSAGE');
+        const reprompt = this.t('WELCOME_REPROMT');
+        this.emit(':ask', speechOutput, reprompt);
+    },
     'GetNewFactIntent': function () {
         this.emit('GetFact');
     },
     'GetFact': function () {
-        // Get a random name from the name list
         const nameIndex = Math.floor(Math.random() * names.length);
-        const randomName = names[nameIndex];
+        const randomName = names[nameIndex].name;
 
         // Create speech output
         const speechOutput = randomName;
         this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomName);
     },
-//	'GetNextEventIntent': function () {
-//		this.emit('GetFact');
-//	}
     'AMAZON.HelpIntent': function () {
         const speechOutput = this.t('HELP_MESSAGE');
         const reprompt = this.t('HELP_MESSAGE');
         this.emit(':ask', speechOutput, reprompt);
     },
+    'AMAZON.NextIntent': function () {
+        this.emit('GetFact');
+    },
+    'AMAZON.PreviousIntent': function () {
+        this.emit('GetFact');
+    },
+    'AMAZON.NoIntent': function () {
+        this.emit('GetFact');
+    },
+    'AMAZON.YesIntent': function () {
+        this.emit('GetFact');
+    },
     'AMAZON.CancelIntent': function () {
-        this.emit('SessionEndedRequest');
+        this.emit(':tell', this.t('STOP_MESSAGE'));
     },
     'AMAZON.StopIntent': function () {
-        this.emit('SessionEndedRequest');
+        this.emit(':tell', this.t('STOP_MESSAGE'));
     },
     'SessionEndedRequest': function () {
         this.emit(':tell', this.t('STOP_MESSAGE'));
     },
 };
 
-exports.handler = function(event, context) {
+exports.handler = (event, context) => {
     const alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
     // To enable string internationalization (i18n) features, set a resources object.
